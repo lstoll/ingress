467015d696d580562a5d5c0cb63b89b97559c7b8
Lincoln Stoll <me@lds.li>
2021-09-01 17:28:48 -0500

    Add autocert, storing keys in kube secret
    This let's us serve with proper TLS certs, using the cluster we're running on
    to save the keys

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ ingress.
 it's a sidecar, that can be the target of an internet facing kubernetes service (i.e elb or nlb)
 
 it handles:
-* TLS via LetsEncrypt
-* Auth via OIDC web flow
-* Auth via OIDC token in header
-* IP Allowlist Enforcement
-* Managed device client cert auth.
-* Passing user info back
+* TLS via LetsEncrypt :white_check_mark:
+* Auth via OIDC web flow :x:
+* Auth via OIDC token in header :x:
+* IP Allowlist Enforcement :x:
+* Managed device client cert auth. :x:
+* Passing user info back :x:
 
 Basically, everything needed to safely expose a service to the internet. It's mostly a simple close to the app hack to get stuff out there fast
--- main.go
+++ main.go
@@ -3,38 +3,59 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/armon/go-proxyproto"
 	"github.com/oklog/run"
+	"golang.org/x/crypto/acme/autocert"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
 	ctx := context.Background()
 
 	cfg := struct {
-		Listen      string
-		UpstreamURL string
+		Listen             string
+		PublicURL          string
+		UpstreamURL        string
+		DisableLetsencrypt bool
+		KubeconfigPath     string
+		Secret             string
 	}{}
 
 	flag.StringVar(&cfg.Listen, "listen", "localhost:8080", "host:port to listen on")
+	flag.StringVar(&cfg.PublicURL, "public-url", "http://localhost", "URL service is served on, for certificate management")
 	flag.StringVar(&cfg.UpstreamURL, "upstream-url", "http://localhost:8080", "URL of upstream service to proxy traffic to")
+	flag.BoolVar(&cfg.DisableLetsencrypt, "disable-letsencrypt", false, "Don't manage certs with letsencrypt. Useful for local dev")
+	flag.StringVar(&cfg.KubeconfigPath, "kubeconfig", filepath.Join(homedir.HomeDir(), ".kube", "config"), "Path to kubeconfig, if not set in-cluster config assumed")
+	flag.StringVar(&cfg.Secret, "secret", "", "namespace/name formatted secret to use for TLS cert storage")
 
 	flag.Parse()
 
-	rpURL, err := url.Parse(cfg.UpstreamURL)
+	usURL, err := url.Parse(cfg.UpstreamURL)
+	if err != nil {
+		log.Fatalf("parsing %s: %v", cfg.UpstreamURL, err)
+	}
+	pURL, err := url.Parse(cfg.UpstreamURL)
 	if err != nil {
 		log.Fatalf("parsing %s: %v", cfg.UpstreamURL, err)
 	}
 
 	mux := http.NewServeMux()
 
-	proxy := httputil.NewSingleHostReverseProxy(rpURL)
+	proxy := httputil.NewSingleHostReverseProxy(usURL)
 	mux.Handle("/", proxy)
 
 	lis, err := net.Listen("tcp", cfg.Listen)
@@ -43,14 +64,66 @@ func main() {
 	}
 	plis := &proxyproto.Listener{Listener: lis}
 
+	hs := &http.Server{
+		Handler: mux,
+	}
+
+	if !cfg.DisableLetsencrypt {
+		if cfg.Secret == "" {
+			log.Fatal("-secret must be provided")
+		}
+
+		var kubeConfig *rest.Config
+		if cfg.KubeconfigPath != "" {
+			c, err := clientcmd.BuildConfigFromFlags("", cfg.KubeconfigPath)
+			if err != nil {
+				log.Fatalf("building kubeconfig from %s: %v", cfg.KubeconfigPath, err)
+			}
+			kubeConfig = c
+		} else {
+			c, err := rest.InClusterConfig()
+			if err != nil {
+				log.Fatalf("building in-cluster kubeconfig: %v", err)
+			}
+			kubeConfig = c
+		}
+		clientset, err := kubernetes.NewForConfig(kubeConfig)
+		if err != nil {
+			log.Fatalf("building kubernetes clientset: %v", err)
+		}
+
+		secretNamespace, secretName, err := splitKubernetesPath(cfg.Secret)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		h, _, err := net.SplitHostPort(pURL.Host)
+		if err != nil {
+			log.Fatalf("splitting %s: %v", pURL.Host, err)
+		}
+		acm := &autocert.Manager{
+			Prompt: autocert.AcceptTOS,
+			Cache: &autocertCache{
+				clientset:       clientset,
+				secretNamespace: secretNamespace,
+				secretName:      secretName,
+			},
+			HostPolicy: autocert.HostWhitelist(h),
+		}
+		hs.TLSConfig = acm.TLSConfig()
+	}
+
 	var g run.Group
 	g.Add(run.SignalHandler(ctx, os.Interrupt))
 
 	g.Add(func() error {
 		log.Printf("Serving on %s", cfg.Listen)
-		return http.Serve(plis, mux)
+		return hs.Serve(plis)
 	}, func(error) {
-		lis.Close()
+		// if we are here context probably already gone. use a new one with a timeout
+		sctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+		hs.Shutdown(sctx)
 	})
 
 	log.Fatal(g.Run())
@@ -58,4 +131,12 @@ func main() {
 		log.Fatalf("running: %v", err)
 	}
 	log.Print("done")
+}
+
+func splitKubernetesPath(p string) (namespace, name string, err error) {
+	sp := strings.Split(p, "/")
+	if len(sp) != 2 {
+		return "", "", fmt.Errorf("splitting %s on / yielded %d items, not 2", p, len(sp))
+	}
+	return sp[0], sp[1], nil
 }
